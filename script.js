6f300140bd62089c5e7841c1db117d8fc56f57dc
B3LSC_Pemulax <95179496+Hironi645@users.noreply.github.com>
2026-03-12 18:50:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ const adminList = [
   { nomor: "62895320390613", nama: "ÐR Cimay", label: "Admin 4", theme: "green", initial: "ÐC" },
   { nomor: "6285824168807", nama: "ÐR SanRa", label: "Admin 5", theme: "orange", initial: "ÐS" },
   { nomor: "6281318685216", nama: "Lucanne", label: "Admin 6", theme: "pink", initial: "LC" },
-  { nomor: "6289504498328", nama: "vhany", label: "Admin 7", theme: "cyan", initial: "VH" }
+  { nomor: "6289504498328", nama: "vhany", label: "Admin 7", theme: "cyan", initial: "VH" } , 
+  { nomor: "6288991037227", nama: "dika", label: "Admin 8", theme: " green", initial: "RH" }
 ];
 
 // ===== ADMIN THEME COLORS =====
